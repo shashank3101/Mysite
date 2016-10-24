9a4c686408d202bed62e0e6c9a944a4c42d97d1c
shashank3101 <shangs93@gmail.com>
2016-10-24 20:05:29 +0530

    sa

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Question
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -13,3 +15,16 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
