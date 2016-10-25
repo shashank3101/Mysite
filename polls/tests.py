f502334ae2fe53c394d957b734a733c5c4c4a988
shashank3101 <shangs93@gmail.com>
2016-10-25 22:38:52 +0530

    sas

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,42 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+        def test_was_published_recently_with_old_question(self):
+
+            time = timezone.now() - datetime.timedelta(days=30)
+            old_question = Question(pub_date=time)
+            self.assertIs(old_question.was_published_recently(), False)
+
+        def test_was_published_recently_with_recent_question(self):
+
+            time = timezone.now() - datetime.timedelta(hours=1)
+            recent_question = Question(pub_date=time)
+            self.assertIs(recent_question.was_published_recently(), True)
+
+            class QuestionIndexDetailTests(TestCase):
+                def test_detail_view_with_a_future_question(self):
+
+                    future_question = create_question(question_text='Future question.', days=5)
+                    url = reverse('polls:detail', args=(future_question.id,))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, 404)
+
+                def test_detail_view_with_a_past_question(self):
+                   
+                    past_question = create_question(question_text='Past Question.', days=-5)
+                    url = reverse('polls:detail', args=(past_question.id,))
+                    response = self.client.get(url)
+                    self.assertContains(response, past_question.question_text)
--- polls/views.py
+++ polls/views.py
@@ -18,6 +18,10 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -60,3 +64,8 @@ def vote(request, question_id):
     def results(request, question_id):
         question = get_object_or_404(Question, pk=question_id)
         return render(request, 'polls/results.html', {'question': question})
+
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
